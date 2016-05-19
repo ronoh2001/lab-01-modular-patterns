9e6a178e8d528bf43da59b061e937d138aad348f
ron oh <ronoh2001@gmail.com>
2016-05-18 17:53:01 -0700

    update

--- lab-ron/test/greet-test.js
+++ lab-ron/test/greet-test.js
@@ -5,9 +5,9 @@ const assert = require('assert');
 
 describe('testing module lab-ron/greet.js', function(){
   describe('testing function greet()', function(){
-    it('should return hello + name', function(){
-      assert.equal(greet(), 'hello Ron Oh');
-      // assert.equal(greet('Yunjoo'), 'hello Yunjoo');
+    it('should return hello + Ron Oh', function(){
+      assert.to.equal(greet(), 'hello Ron Oh');
+      // process.argv=['node', 'index.js', 'Ron Oh'];
     });
   });
 });
